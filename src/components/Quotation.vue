ec6957dcd55d8245ef140b16097d36498cb080cb
KA-DEVL <anes1999@live.ca>
2023-11-06 20:44:13 -0800

    Payment section completed

--- src/components/Quotation.vue
+++ src/components/Quotation.vue
@@ -13,21 +13,26 @@
     <div class="relative">
       <ul class="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
         <li class="flex items-center space-x-3 text-left sm:space-x-4">
-            <a class="flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 text-xs font-semibold text-white ring ring-blue-500 ring-offset-2" href="#">1</a>
+            <a v-if="isEstimation" class="flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 text-xs font-semibold text-white ring ring-blue-500 ring-offset-2" href="#">1</a>
+            <a v-if="!isEstimation" class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-500 text-xs font-semibold text-white ring ring-gray-500 ring-offset-2" href="#">1</a>
+
           <span class="font-semibold text-gray-900">Estimation</span>
         </li>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
         </svg>
         <li class="flex items-center space-x-3 text-left sm:space-x-4">
-          <a class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2" href="#">2</a>
+          <a v-if="isPayment" class="flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 text-xs font-semibold text-white ring ring-blue-600 ring-offset-2" href="#">2</a>
+          <a v-if="!isPayment" class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-500 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2" href="#">2</a>
+
           <span class="font-semibold text-gray-900">Payment</span>
         </li>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
         </svg>
         <li class="flex items-center space-x-3 text-left sm:space-x-4">
-          <a class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white" href="#">3</a>
+         <a v-if="isConfirmation" class="flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 text-xs font-semibold text-white ring ring-blue-600 ring-offset-2" href="#">3</a>
+          <a v-if="!isConfirmation" class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-500 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2" href="#">3</a>
           <span class="font-semibold text-gray-500">Confirmation</span>
         </li>
       </ul>
@@ -260,7 +265,7 @@
     <div class="">
       <label for="email" class="mt-4 mb-2 block text-sm font-medium">Email</label>
       <div class="relative">
-        <input type="text" id="email" name="email" class="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="your.email@gmail.com" />
+        <input type="text" v-model="emailBilling" id="email" name="email" class="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="your.email@gmail.com" />
         <div class="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
@@ -269,7 +274,7 @@
       </div>
       <label for="card-holder" class="mt-4 mb-2 block text-sm font-medium">Card Holder</label>
       <div class="relative">
-        <input type="text" id="card-holder" name="card-holder" class="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Your full name here" />
+        <input type="text" v-model="nameBilling" id="card-holder" name="card-holder" class="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Your full name here" />
         <div class="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z" />
@@ -279,7 +284,7 @@
       <label for="card-no" class="mt-4 mb-2 block text-sm font-medium">Card Details</label>
       <div class="flex">
         <div class="relative w-7/12 flex-shrink-0">
-          <input type="text" id="card-no" name="card-no" class="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="xxxx-xxxx-xxxx-xxxx" />
+          <input type="text" v-model="cardNumberBilling" id="card-no" name="card-no" class="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="xxxx-xxxx-xxxx-xxxx" />
           <div class="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
             <svg class="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
               <path d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z" />
@@ -287,21 +292,8 @@
             </svg>
           </div>
         </div>
-        <input type="text" name="credit-expiry" class="w-full rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="MM/YY" />
-        <input type="text" name="credit-cvc" class="w-1/6 flex-shrink-0 rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="CVC" />
-      </div>
-      <label for="billing-address" class="mt-4 mb-2 block text-sm font-medium">Billing Address</label>
-      <div class="flex flex-col sm:flex-row">
-        <div class="relative flex-shrink-0 sm:w-7/12">
-          <input type="text" id="billing-address" name="billing-address" class="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Street Address" />
-          <div class="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-            <img class="h-4 w-4 object-contain" src="https://flagpack.xyz/_nuxt/4c829b6c0131de7162790d2f897a90fd.svg" alt="" />
-          </div>
-        </div>
-        <select type="text" name="billing-state" class="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500">
-          <option value="State">State</option>
-        </select>
-        <input type="text" name="billing-zip" class="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/6 focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="ZIP" />
+        <input type="text" v-model="cardExpiryBilling" name="credit-expiry" class="w-full rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="MM/YY" />
+        <input type="text" v-model="cardCvcBilling" name="credit-cvc" class="w-1/6 flex-shrink-0 rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="CVC" />
       </div>
 
       <!-- Total -->
@@ -320,7 +312,7 @@
         <p class="text-2xl font-semibold text-gray-900">${{ totalAfterTax + 8 }}0</p>
       </div>
     </div>
-    <button class="mt-4 mb-8 w-full rounded-md bg-blue-500 px-6 py-3 font-medium text-white">Place Order</button>
+    <button @click="placeOrder" class="mt-4 mb-8 w-full rounded-md bg-blue-500 px-6 py-3 font-medium text-white">Place Order</button>
   </div>
 </div>
 
@@ -352,6 +344,7 @@ export default {
     const weight = ref();
     const isPayment = ref(false);
     const isEstimation = ref(true);
+    const isConfirmation = ref(false);
 
     const nameSender = ref();
     const addressSender = ref();
@@ -363,7 +356,40 @@ export default {
     const cityReceiver = ref();
     const numberReceiver = ref();
 
-    const verifyFields = () => {
+    //Billing
+      const nameBilling = ref();
+      const emailBilling = ref();
+      const cardNumberBilling = ref();
+      const cardExpiryBilling = ref();
+      const cardCvcBilling = ref();
+
+      const verifyPaymentFields = () => {
+      if (
+        nameBilling.value &&
+        emailBilling.value &&
+        cardNumberBilling.value &&
+        cardExpiryBilling.value &&
+        cardCvcBilling.value
+      ) {
+        return true;
+      } else {
+        return false;
+      }  
+   };
+
+   const placeOrder = () => {
+      if (verifyPaymentFields()) {
+        isPayment.value = false;
+        isConfirmation.value = true;
+      } else {
+        toast.error("Please fill all the fields", {
+          timeout: 4000,
+        });
+      }
+    };
+   
+
+    const verifyEstimationFields = () => {
       if (
         height.value &&
         width.value &&
@@ -385,7 +411,7 @@ export default {
     };
 
     const SendPackage = () => {
-      if (verifyFields()) {
+      if (verifyEstimationFields()) {
         isEstimation.value = false;
          isPayment.value = true;
       } else {
@@ -425,7 +451,14 @@ export default {
       numberReceiver,
       SendPackage,
       isPayment,
-      isEstimation
+      isEstimation,
+      isConfirmation,
+      nameBilling,
+      emailBilling,
+      cardNumberBilling,
+      cardExpiryBilling,
+      cardCvcBilling,
+      placeOrder
     };
   },
 };
