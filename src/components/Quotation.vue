949781fe7546b38df0bdd6cac3e222c477b9e827
KA-DEVL <anes1999@live.ca>
2023-11-06 21:00:54 -0800

    Finishing invoice

--- src/components/Quotation.vue
+++ src/components/Quotation.vue
@@ -317,6 +317,149 @@
 </div>
 
 
+<!-- INVOICE -->
+
+
+<div v-if="isConfirmation" class=" dark:bg-slate-900">
+  <!-- Invoice -->
+  <div class="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto my-4 sm:my-10">
+    <div class="sm:w-11/12 lg:w-3/4 mx-auto">
+      <!-- Card -->
+      <div class="flex flex-col p-4 sm:p-10 bg-white shadow-md rounded-xl dark:bg-gray-800">
+        <!-- Grid -->
+        <div class="flex justify-between">
+          <div>
+            <img src="../assets/turbopost.png" alt="logo" >
+          </div>
+          <!-- Col -->
+
+          <div class="text-end">
+            <h2 class="text-2xl md:text-3xl font-semibold text-gray-800 dark:text-gray-200">Invoice #</h2>
+            <span class="mt-1 block text-gray-500">3682303</span>
+
+            <address class="mt-4 not-italic text-gray-800 dark:text-gray-200">
+              45 Roker Terrace<br>
+              Latheronwheel<br>
+              KW5 8NW, London<br>
+              United Kingdom<br>
+            </address>
+          </div>
+          <!-- Col -->
+        </div>
+        <!-- End Grid -->
+
+        <!-- Grid -->
+        <div class="mt-8 grid sm:grid-cols-2 gap-3">
+          <div>
+            <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200">Bill to:</h3>
+            <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200">{{ nameBilling }}</h3>
+          </div>
+          <!-- Col -->
+
+     
+          <!-- Col -->
+        </div>
+        <!-- End Grid -->
+
+        <!-- Table -->
+        <div class="mt-6">
+          <div class="border border-gray-200 p-4 rounded-lg space-y-4 dark:border-gray-700">
+            <div class="hidden sm:grid sm:grid-cols-5">
+              <div class="sm:col-span-2 text-xs font-medium text-gray-500 uppercase">Item</div>
+              <div class="text-start text-xs font-medium text-gray-500 uppercase">Qty</div>
+              <div class="text-start text-xs font-medium text-gray-500 uppercase">Package size</div>
+              <div class="text-end text-xs font-medium text-gray-500 uppercase">Amount</div>
+            </div>
+
+            <div class="hidden sm:block border-b border-gray-200 dark:border-gray-700"></div>
+
+            <div class="grid grid-cols-3 sm:grid-cols-5 gap-2">
+              <div class="col-span-full sm:col-span-2">
+                <h5 class="sm:hidden text-xs font-medium text-gray-500 uppercase">Item</h5>
+                <p class="font-medium text-gray-800 dark:text-gray-200">PACKAGE</p>
+              </div>
+              <div>
+                <h5 class="sm:hidden text-xs font-medium text-gray-500 uppercase">Qty</h5>
+                <p class="text-gray-800 dark:text-gray-200">1</p>
+              </div>
+              <div>
+                <h5 class="sm:hidden text-xs font-medium text-gray-500 uppercase">Rate</h5>
+                <p class="text-gray-800 dark:text-gray-200">{{ height }}cm x {{ width }}cm x {{ length }}cm x {{ weight }}kg</p>
+              </div>
+              <div>
+                <h5 class="sm:hidden text-xs font-medium text-gray-500 uppercase">Amount</h5>
+                <p class="sm:text-end text-gray-800 dark:text-gray-200">${{ totalBeforeTax }}</p>
+              </div>
+            </div>
+
+            <div class="sm:hidden border-b border-gray-200 dark:border-gray-700"></div>
+
+            
+
+            <div class="sm:hidden border-b border-gray-200 dark:border-gray-700"></div>
+
+           
+          </div>
+        </div>
+        <!-- End Table -->
+
+        <!-- Flex -->
+        <div class="mt-8 flex sm:justify-end">
+          <div class="w-full max-w-2xl sm:text-end space-y-2">
+            <!-- Grid -->
+            <div class="grid grid-cols-2 sm:grid-cols-1 gap-3 sm:gap-2">
+              <dl class="grid sm:grid-cols-5 gap-x-3">
+                <dt class="col-span-3 font-semibold text-gray-800 dark:text-gray-200">Subtotal:</dt>
+                <dd class="col-span-2 text-gray-500">${{ totalBeforeTax }}0</dd>
+              </dl>
+
+              <dl class="grid sm:grid-cols-5 gap-x-3">
+                <dt class="col-span-3 font-semibold text-gray-800 dark:text-gray-200">Total:</dt>
+                <dd class="col-span-2 text-gray-500">${{ totalAfterTax }}0</dd>
+              </dl>
+
+              <dl class="grid sm:grid-cols-5 gap-x-3">
+                <dt class="col-span-3 font-semibold text-gray-800 dark:text-gray-200">Tax:</dt>
+                <dd class="col-span-2 text-gray-500">${{ taxAmount }}0</dd>
+              </dl>
+
+            </div>
+            <!-- End Grid -->
+          </div>
+        </div>
+        <!-- End Flex -->
+
+        <div class="mt-8 sm:mt-12">
+          <h4 class="text-lg font-semibold text-gray-800 dark:text-gray-200">Thank you!</h4>
+          <p class="text-gray-500">If you have any questions concerning this invoice, use the following contact information:</p>
+          <div class="mt-2">
+            <p class="block text-sm font-medium text-gray-800 dark:text-gray-200">contact@turbopost.com</p>
+            <p class="block text-sm font-medium text-gray-800 dark:text-gray-200">+1 (062) 109-9222</p>
+          </div>
+        </div>
+
+        <p class="mt-5 text-sm text-gray-500">© 2023 TurboPost.</p>
+      </div>
+      <!-- End Card -->
+
+      <!-- Buttons -->
+      <div class="mt-6 flex justify-end gap-x-3">
+        <a class="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-lg border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" href="#">
+          <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" x2="12" y1="15" y2="3"/></svg>
+          Invoice PDF
+        </a>
+        <a class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+          <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="6 9 6 2 18 2 18 9"/><path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"/><rect width="12" height="8" x="6" y="14"/></svg>
+          Print
+        </a>
+      </div>
+      <!-- End Buttons -->
+    </div>
+  </div>
+  <!-- End Invoice -->
+</div>
+
+
   <br />
   <br />
   <br />
@@ -381,6 +524,9 @@ export default {
       if (verifyPaymentFields()) {
         isPayment.value = false;
         isConfirmation.value = true;
+        toast.success("Order placed successfully", {
+          timeout: 4000,
+        });
       } else {
         toast.error("Please fill all the fields", {
           timeout: 4000,
