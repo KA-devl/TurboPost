cb066a315db9202e73cb7f618104c144f1ec0a8b
KA-DEVL <anes1999@live.ca>
2023-11-15 17:25:05 -0800

    Fixing footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,7 @@
   <!-- Grid -->
   <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 mb-10">
     <div class="col-span-full hidden lg:col-span-1 lg:block">
-      <a class="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">Brand</a>
-      <p class="mt-3 text-xs sm:text-sm text-gray-600 dark:text-gray-400">© 2022 Preline.</p>
+      <p class="mt-3 text-xs sm:text-sm text-gray-600 dark:text-gray-400">© 2023 TurboPost.</p>
     </div>
     <!-- End Col -->
 
